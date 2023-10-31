23b6bd795638b5e9e86a6820dfd40f3e4e52d538
pushssun <alsgp25@naver.com>
2023-10-31 18:45:21 +0900

    UI Update
    ui 버전 수정

--- Assets/Scripts/Manager/GameManager.cs
+++ Assets/Scripts/Manager/GameManager.cs
@@ -7,9 +7,7 @@ public class GameManager : SingletonHandler<GameManager>
     public bool IsStart;
     public bool IsFirstStart;
 
-    //public Player1 Player1;
     public Player Player;
-    //public UIMessage1 UIMessage; //삭제 예정
     public MessageDatabase MessageDatabase;
 
 }
