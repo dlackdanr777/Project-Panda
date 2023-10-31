21ace35a6577cbb1ac425e779faaeff54b4ccdeb
dlackdanr777 <64966089+dlackdanr777@users.noreply.github.com>
2023-10-31 23:51:41 +0900

    Error Occurrence
    내일 다시 수정예정

--- Assets/Scripts/Cameara app/CameraApplication.cs
+++ Assets/Scripts/Cameara app/CameraApplication.cs
@@ -15,23 +15,29 @@ public class CameraApplication : MonoBehaviour
     [Tooltip("캡쳐 영역을 표시할 이미지 오브젝트")]
     [SerializeField] private AreaImage _areaImage;
 
+    [Tooltip("사진 저장소 추가하기")]
+    [SerializeField] private PhotoDatabase _photoDatabase;
+
+    [SerializeField] private UIPhotoAlbum _photoAlbum;
+
     private Camera _camera => Camera.main;
+
+   
 
     //====================================================//
     [Space(50)]
 
     [Tooltip("자석 모드 활성/비활성")]
-    [SerializeField] private bool _isMagnetMode;
+    [SerializeField] private bool _isMagnetEnable;
 
     [Tooltip("자석 기능의 감지 범위")]
     [SerializeField] private Vector3 _boxSize;
 
-    private bool _isLerpStart;
-
+    [SerializeField]  private bool _isMagnetMode;
     private void Awake()
     {
-        _areaImage.OnPointerDownHandler += () => _isMagnetMode = false;
-        _areaImage.OnPointerUpHandler += () => _isMagnetMode = true;
+        _areaImage.OnPointerDownHandler += () => { if (_isMagnetEnable) _isMagnetMode = false; };
+        _areaImage.OnPointerUpHandler += () => { if (_isMagnetEnable) _isMagnetMode = true; };
     }
 
 
@@ -73,7 +79,17 @@ public class CameraApplication : MonoBehaviour
         ss.Apply();
 
         //텍스처를 PNG 형식으로 인코딩하여 byte배열에 저장
+        
         byte[] byteArray = ss.EncodeToPNG();
+
+        //여기부터 테스트
+        PhotoData photoData = new PhotoData("사진", "사진입니다.", "2023.09.23", ss);
+
+        _photoDatabase.AddPhotoData(photoData);
+
+        _photoAlbum.GetImage().sprite = _photoDatabase.SetPhotoDataToSprite(0);
+        //여기까지 테스트
+
 
         //저장 위치 지정
         string savePath = Application.persistentDataPath + "/ScreenshotSave.png";
@@ -94,10 +110,10 @@ public class CameraApplication : MonoBehaviour
         MagnetFunction();
     }
 
-
+    //특정 오브젝트에 범위가 달라붙게 하는 함수
     private void MagnetFunction()
     {
-        if (!_isMagnetMode || _isLerpStart)
+        if (!_isMagnetMode || !_isMagnetEnable)
             return;
 
         Vector3 imageWorldPos = _camera.ScreenToWorldPoint(_areaImage.transform.position);
@@ -119,21 +135,22 @@ public class CameraApplication : MonoBehaviour
 
     private IEnumerator ImagePosLerp(Vector2 startPos, Vector2 endPos, float duration )
     {
-        _isLerpStart = true;
-        float timer = 0;
-
-        while (timer < duration)
+        if(_isMagnetEnable)
         {
-            timer += Time.deltaTime;
+            float timer = 0;
 
-            float t = timer / duration;
-            t = t * t * (3f - 2f * t);
+            while (timer < duration)
+            {
+                timer += Time.deltaTime;
 
-            _areaImage.transform.position = Vector2.Lerp(startPos, endPos, t);
+                float t = timer / duration;
+                t = t * t * (3f - 2f * t);
 
-            yield return null;
+                _areaImage.transform.position = Vector2.Lerp(startPos, endPos, t);
+
+                yield return null;
+            }
         }
-        _isLerpStart = false;
     }
 
     private Vector3[] GetIamgeCorners(Image image)
