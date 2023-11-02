aff7e379fe9a92b92b4dcfd77980a6e2539c6c68
pushssun <alsgp25@naver.com>
2023-11-02 19:46:34 +0900

    Inventory UI 생성
    Inventory UI 생성
    UIManager 생성

--- Assets/Scripts/Message/UIMessage.cs
+++ Assets/Scripts/Message/UIMessage.cs
@@ -2,62 +2,46 @@ using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class UIMessage : MonoBehaviour
+public class UIMessage : UIManager
 {
     [SerializeField]
-    private Button _messageButton;
-    [SerializeField]
     private Button _removeButton;
-    [SerializeField]
-    private GameObject _messagePf;
     [SerializeField]
     private GameObject _messageList;
     [SerializeField]
     private GameObject _notice;
     [SerializeField]
     private GameObject _removePopup;
-    [SerializeField]
-    private Transform _instantiatePosition;
 
-    private Player _player;
     private SendMessage _sendMessage;
-
-    public GameObject MessageView;
 
     // Start is called before the first frame update
     void Start()
     {
-        _player = GameManager.Instance.Player;
         _sendMessage = GetComponent<SendMessage>();
 
-        for (int i = 0; i < _player.MaxMessageCount; i++)
+        for (int i = 0; i < Player.MaxMessageCount; i++)
         {
-            GameObject content = Instantiate(_messagePf, _instantiatePosition);
-            content.GetComponent<MessageDetail>().NoticeHandler += SetNotice;
+           
+            _prefab.GetComponent<MessageDetail>().NoticeHandler += SetNotice;
         }
 
-        _messageButton.onClick.AddListener(OnClickMessageButton);
         _removeButton.onClick.AddListener(OnClickRemoveButton);
         _sendMessage.NoticeHandler += SendMessage_NoticeHandler;
         _removePopup.transform.Find("YesButton").GetComponent<Button>().onClick.AddListener(OnRemoveMessage);
         
     }
-
+ 
     private void SendMessage_NoticeHandler()
     {
         OnChangeaReveivedMessage();
     }
-    
-    private void OnClickMessageButton()
-    {
-        _messageList.gameObject.SetActive(true);
-    }
 
     private void OnClickRemoveButton()
     {
-        for (int i = 0; i < _player.MaxMessageCount; i++)
+        for (int i = 0; i < Player.MaxMessageCount; i++)
         {
-            GameObject message = _instantiatePosition.GetChild(i).gameObject; //message, 위치
+            GameObject message = SpawnPoint.GetChild(i).gameObject; //message, 위치
             if (message.activeSelf)
             {
                 if (message.transform.Find("ClickMessage").GetComponent<Toggle>().isOn)
@@ -71,17 +55,17 @@ public class UIMessage : MonoBehaviour
 
     private void OnRemoveMessage()
     {
-        for (int i = 0; i < _player.MaxMessageCount ; i++)
+        for (int i = 0; i < Player.MaxMessageCount ; i++)
         {
-            GameObject message = _instantiatePosition.GetChild(i).gameObject; //message, 위치
+            GameObject message = SpawnPoint.GetChild(i).gameObject; //message, 위치
             if (message.activeSelf)
             {
                 if (message.transform.Find("ClickMessage").GetComponent<Toggle>().isOn)
                 {
                     message.transform.Find("ClickMessage").GetComponent<Toggle>().isOn = false;
-                    _player.IsCheckMessage.RemoveAt(_player.MaxMessageCount - 1 - i); //마지막 자리에서부터 삭제           
-                    _player.IsReceiveGift.RemoveAt(_player.MaxMessageCount - 1 - i);         
-                    _player.Messages.RemoveAt(_player.MaxMessageCount - 1 - i);
+                    Player.IsCheckMessage.RemoveAt(Player.MaxMessageCount - 1 - i); //마지막 자리에서부터 삭제           
+                    Player.IsReceiveGift.RemoveAt(Player.MaxMessageCount - 1 - i);         
+                    Player.Messages.RemoveAt(Player.MaxMessageCount - 1 - i);
 
                 }
 
@@ -95,29 +79,29 @@ public class UIMessage : MonoBehaviour
 
     private void OnChangeaReveivedMessage()
     {
-        int index = _player.MaxMessageCount - _player.Messages.Count;
+        int index = Player.MaxMessageCount - Player.Messages.Count;
 
         SetNotice();
 
         for (int i = 0; i < index; i++)
         {
-            _instantiatePosition.GetChild(i).gameObject.SetActive(false);
+            SpawnPoint.GetChild(i).gameObject.SetActive(false);
         }
-        if (_player.Messages.Count > 0)
+        if (Player.Messages.Count > 0)
         {
             //문자 UI
-            for (int i = index; i < _player.MaxMessageCount; i++)
+            for (int i = index; i < Player.MaxMessageCount; i++)
             {
-                _instantiatePosition.GetChild(i).gameObject.SetActive(true); //스크롤 뷰의 자식 setActive true
-                _instantiatePosition.GetChild(i).transform.GetChild(1).GetComponent<TextMeshProUGUI>().text = _player.Messages[_player.MaxMessageCount- 1 -i].From; //text를 받은 문자로 변환
-                _instantiatePosition.GetChild(i).transform.GetChild(2).gameObject.SetActive(!_player.IsCheckMessage[_player.MaxMessageCount - 1 - i]); //문자 확인 이미지 변경
+                SpawnPoint.GetChild(i).gameObject.SetActive(true); //스크롤 뷰의 자식 setActive true
+                SpawnPoint.GetChild(i).transform.GetChild(1).GetComponent<TextMeshProUGUI>().text = Player.Messages[Player.MaxMessageCount- 1 -i].From; //text를 받은 문자로 변환
+                SpawnPoint.GetChild(i).transform.GetChild(2).gameObject.SetActive(!Player.IsCheckMessage[Player.MaxMessageCount - 1 - i]); //문자 확인 이미지 변경
             }
         }
     }
 
     private void SetNotice() //알림 설정, 알림 갯수 설정 //UI로 가야할 것 같음
     {
-        if (_player.CurrentNotCheckedMessage == 0)
+        if (Player.CurrentNotCheckedMessage == 0)
         {
             _notice.SetActive(false);
         }
@@ -125,7 +109,7 @@ public class UIMessage : MonoBehaviour
         {
             //알림 보이기
             _notice.SetActive(true);
-            _notice.transform.GetChild(0).GetComponent<TextMeshProUGUI>().text = _player.CurrentNotCheckedMessage.ToString();// 알림 갯수 변경
+            _notice.transform.GetChild(0).GetComponent<TextMeshProUGUI>().text = Player.CurrentNotCheckedMessage.ToString();// 알림 갯수 변경
         }
     }
 }
