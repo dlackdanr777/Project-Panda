868c9dd56d177d9fe954b4f927ed49a09200f7f6
pushssun <alsgp25@naver.com>
2023-11-14 17:07:39 +0900

    Update UIInventoryList.cs

--- Assets/Scripts/Inventory/UIInventoryList.cs
+++ Assets/Scripts/Inventory/UIInventoryList.cs
@@ -47,8 +47,9 @@ public class UIInventoryList : UIList<InventoryItem>
 
         }
 
-        _arrangeButton.GetComponent<DragAndDrop>().DontUseItem -= UIInventoryList_DontUseItem;
-        _arrangeButton.GetComponent<DragAndDrop>().OnUseItem -= UIInventoryList_OnUseItem;
+        DataBind.SetTextValue("InventoryDetailName", _lists[(int)_currentField][index].Name);
+        DataBind.SetTextValue("InventoryDetailDescription", _lists[(int)_currentField][index].Description);
+        DataBind.SetSpriteValue("InventoryDetailImage", _lists[(int)_currentField][index].Image);
     }
 
     private void UIInventoryList_OnUseItem() //æ∆¿Ã≈€ 
