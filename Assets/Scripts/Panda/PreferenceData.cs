588028cbc72278e196a1b793822160a04238e31b
dlackdanr777 <64966089+dlackdanr777@users.noreply.github.com>
2023-11-22 11:26:14 +0900

    Revert "Merge pull request #39 from dlackdanr777/hye"
    This reverts commit 315449a84c4953a21710220f37abfff5b6261121, reversing
    changes made to ad56410a612a355d8533540091a2f270a1e51823.

--- Assets/Scripts/Panda/PreferenceData.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class PreferenceData
-{
-    public string FavoriteToy;
-    public string FavoriteSnack;
-}
