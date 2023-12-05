f787277ef534c2b5ef5733ada650a5cc35c400d7
pushssun <alsgp25@naver.com>
2023-12-05 10:59:42 +0900

    Revert "inventory to world position"
    This reverts commit e5b3cc07b660c671869c31a745da9bfab6298870.

--- Assets/Scripts/Inventory/DragDrop.cs
+++ Assets/Scripts/Inventory/DragDrop.cs
@@ -1,6 +1,5 @@
 using UnityEngine;
 using UnityEngine.EventSystems;
-using UnityEngine.UIElements;
 
 public class DragDrop : MonoBehaviour, IBeginDragHandler, IEndDragHandler, IDragHandler
 {
@@ -28,6 +27,7 @@ public class DragDrop : MonoBehaviour, IBeginDragHandler, IEndDragHandler, IDrag
     {
         transform.parent = transform.root; //맨 위로 가도록
         _rectTransform.anchoredPosition += eventData.delta / _canvas.scaleFactor;
+
     }
 
     public void OnEndDrag(PointerEventData eventData)
@@ -35,8 +35,6 @@ public class DragDrop : MonoBehaviour, IBeginDragHandler, IEndDragHandler, IDrag
         _canvasGroup.blocksRaycasts = true;
         transform.parent = _oldParent;
         transform.position = _oldPosition;
-
-        transform.position = Camera.main.ScreenToWorldPoint(Input.mousePosition + new Vector3(0, 0, 10.0f));
-        Debug.Log(transform.position);
+        
     }
 }
