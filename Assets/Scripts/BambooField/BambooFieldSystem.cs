1e65125f1f919309ad695febe4a0524a9002a727
hye <minji11014@gmail.com>
2023-12-14 11:13:08 +0900

    BambooField
    BambooField

--- Assets/Scripts/BambooField/BambooFieldSystem.cs
+++ Assets/Scripts/BambooField/BambooFieldSystem.cs
@@ -31,6 +31,7 @@ public class BambooFieldSystem : MonoBehaviour
     public HarvestButton HarvestButton;
     private Vector3 _targetPos;
 
+    [SerializeField] private GameObject _harvestBamboos;
     [SerializeField] private GameObject _harvestBamboo;
     private GameObject[] _bambooPrefabs = new GameObject[50];
     private int _bambooPrefabCount;
@@ -53,6 +54,10 @@ public class BambooFieldSystem : MonoBehaviour
         {
             _fieldSlots[i].Init(this, 0);// 0: 우선 죽순ID로 설정
         }
+        for(int i = 0; i < _bambooPrefabs.Length; i++)
+        {
+            _bambooPrefabs[i] = Instantiate(_harvestBamboo, _harvestBamboos.transform);
+        }
     }
 
 
@@ -69,22 +74,6 @@ public class BambooFieldSystem : MonoBehaviour
         _targetPos = Camera.main.ScreenToWorldPoint(_UIBamboo.transform.position);
         for (int i = 0; i <= _fieldIndex; i++)
         {
-
-            // 애니메이션 - 죽순이 수집되며 우측 상단의 죽순보유량으로 빨려들어가는 느낌의 애니메이션, 하나씩 빨려들어갈 때마다 죽순량이 동적으로 변화
-            //for (int j = 0; j < _fieldSlots[i].Yield; j++)
-            //{
-            //    int count = _bambooPrefabCount;
-            //    _bambooPrefabs[count] = Instantiate(_harvestBamboo);
-            //    _bambooPrefabs[count].transform.position = _fieldSlots[i].transform.position + new Vector3(UnityEngine.Random.Range(-1f, 1f), UnityEngine.Random.Range(-1f, 1f), 0);
-            //    _bambooPrefabs[count].GetComponent<Animator>().enabled = true;
-
-            //    Tween.TransformMove(_bambooPrefabs[count], _targetPos, 1.5f, TweenMode.Quadratic, () =>
-            //    {
-            //        _player.GainBamboo(1);
-            //        Destroy(_bambooPrefabs[count]);
-            //    });
-            //    _bambooPrefabCount = (_bambooPrefabCount + 1) % 20;
-            //}
             HarvestBamboo(0, _fieldSlots[i].Yield, i);
             _fieldSlots[i].Yield = 0;
 
@@ -101,8 +90,8 @@ public class BambooFieldSystem : MonoBehaviour
     {
         int count = _bambooPrefabCount;
         _bambooPrefabCount = (_bambooPrefabCount + 1) % _bambooPrefabs.Length;
-        _bambooPrefabs[count] = Instantiate(_harvestBamboo);//처음 한 번만 하기
         _bambooPrefabs[count].transform.position = _fieldSlots[fieldSlotNum].transform.position;
+        _bambooPrefabs[count].SetActive(true);
         Vector3 addPosition = new Vector3(UnityEngine.Random.Range(-1f, 1f), UnityEngine.Random.Range(-1f, 1f), 0);
         _bambooPrefabs[count].GetComponent<Animator>().enabled = true;
 
@@ -118,7 +107,7 @@ public class BambooFieldSystem : MonoBehaviour
         Tween.TransformMove(_bambooPrefabs[count], _targetPos, 2, TweenMode.Quadratic, () =>
         {
             _player.GainBamboo(1);
-            Destroy(_bambooPrefabs[count]);//SetActive로 변경
+            _bambooPrefabs[count].SetActive(false);
         });
     }
 
@@ -131,12 +120,10 @@ public class BambooFieldSystem : MonoBehaviour
             //현재 접속 시간과 마지막 접속 시간을 비교
             if ((UserInfo.TODAY - UserInfo._lastAccessDay).Minutes >= _fieldSlots[i].GrowthTime * 2)
             {
-                Debug.Log("작물 2단계 성장");
                 _fieldSlots[i].GrowingCrops(2);
             }
             else if ((UserInfo.TODAY - UserInfo._lastAccessDay).Minutes >= _fieldSlots[i].GrowthTime)
             {
-                Debug.Log("작물 1단계 성장");
                 _fieldSlots[i].GrowingCrops(1);
             }
         }
