0a54ed4786271d77980ec4b803d74ed1f9de9350
hye20 <minji11014@gmail.com>
2023-12-19 03:32:29 +0900

    Revert "Revert "Costume""
    This reverts commit 384ef21d33470860c8c9f133d759ee1e5f791bc7.

---
+++ Assets/Scripts/Costume/CostumeViewModel.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CostumeViewModel : MonoBehaviour
+{
+    
+    private CostumeModel _costumeModel;
+
+    void Start()
+    {
+        _costumeModel = new CostumeModel();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
