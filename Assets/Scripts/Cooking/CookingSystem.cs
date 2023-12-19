0f7804d2506cdc878b86c4e27d08504887bd5f15
dlackdanr777 <64966089+dlackdanr777@users.noreply.github.com>
2023-12-19 09:53:19 +0900

    [Feat] 요리 시스템 업로드

---
+++ Assets/Scripts/Cooking/CookingSystem.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CookingSystem : MonoBehaviour
+{
+    private ItemDatabase _itemDatabase => DatabaseManager.Instance.ItemDatabase;
+
+
+}
---
+++ Assets/Scripts/Cooking/RecipeDatabase.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RecipeData
+{
+    private string _materialItemID;
+    public string MaterialItemID => _materialItemID;
+
+    private int _materialValue;
+    public int MaterialValue => _materialValue;
+
+    private string _cookingItemIDLevel_B;
+    public string CookingItemIDLevel_B => _cookingItemIDLevel_B;
+    private string _cookingItemIDLevel_A;
+    public string CookingItemIDLevel_A => _cookingItemIDLevel_A;
+    private string _cookingItemIDLevel_S;
+    public string CookingItemIDLevel_S => _cookingItemIDLevel_S;
+
+    public RecipeData(string materialItemID, int materialValue,
+        string cookingItemIDLevel_B, string cookingItemIDLevel_A, string cookingItemIDLevel_S)
+    {
+        _materialItemID = materialItemID;
+        _materialValue = materialValue;
+        _cookingItemIDLevel_B = cookingItemIDLevel_B;
+        _cookingItemIDLevel_A = cookingItemIDLevel_A;
+        _cookingItemIDLevel_S = cookingItemIDLevel_S;
+    }
+
+}
+
+public class RecipeDatabase
+{
+    private List<RecipeData> _recipeDataList = new List<RecipeData>();
+
+    private DialogueParser _parser = new DialogueParser();
+
+    public void Register()
+    {
+        DataParse();
+    }
+
+    private void DataParse()
+    {
+        RecipeData[] recipeDatas = _parser.RecipeDataParse("Recipes");
+
+        for (int i = 0, count = recipeDatas.Length; i < count; i++)
+        {
+            _recipeDataList.Add(recipeDatas[i]);
+        }
+    }
+}
