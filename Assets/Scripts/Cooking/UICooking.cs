146c60383d3af1c0d92a38844abe0be4ec395f02
dlackdanr777 <64966089+dlackdanr777@users.noreply.github.com>
2024-01-05 16:12:31 +0900

    [Add] 충돌 부분 수정

--- Assets/Scripts/Cooking/UICooking.cs
+++ Assets/Scripts/Cooking/UICooking.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -17,22 +18,40 @@ public class UICooking : UIView
     [SerializeField] private UICookingDragSlot _uiCookingDragSlot;
     public UICookingDragSlot UICookingDragSlot => _uiCookingDragSlot;
 
-    [SerializeField] private UICookingCenterSlot _uiCookingCenterSlot;
+    [SerializeField] private UICookingCenterSlot[] _uiCookingCenterSlot;
+    public UICookingCenterSlot[] UICookingCenterSlot => _uiCookingCenterSlot;
+
+    private List<UICookingCenterSlot> _uiCookingCenterSlotList = new List<UICookingCenterSlot>();
+
+    [SerializeField] private UICookwares _uiCookwares;
+
+    [Space]
+
+    [SerializeField] private Button _cookButton;
 
     [SerializeField] private GameObject _hideButtonImage;
     public GameObject HideButtonImage => _hideButtonImage;
 
-    private Inventory[] _inventory => GameManager.Instance.Player.CookItemInventory;
+    [SerializeField] private Button _leftCookwareChangeButton;
+
+    [SerializeField] private Button _rightCookwareChangeButton;
+
+    private Inventory[] _inventory => GameManager.Instance.Player.Inventory;
 
     private RecipeData _currentRecipeData;
+
 
 
     public void Start()
     {
         Init(null);
         _uiCookingDragSlot.Init(this);
-        _uiCookingCenterSlot.Init(this);
 
+        for(int i = 0, count = _uiCookingCenterSlot.Length; i < count; i++)
+        {
+            _uiCookingCenterSlot[i].Init(this);
+        }
+        _cookButton.onClick.AddListener(StartCooking);
         _hideButtonImage.SetActive(true);
     }
 
@@ -40,10 +59,10 @@ public class UICooking : UIView
     public override void Init(UINavigation uiNav)
     {
         base.Init(uiNav);
-
         _uiCookingSlots = _uiCookingSlotParent.GetComponentsInChildren<UICookingSlot>();
 
         _uiCookingStart.Init(_cookingSystem, this);
+        _uiCookwares.Init();
 
         for (int i = 0, count = _uiCookingSlots.Length; i < count; i++)
         {
@@ -53,8 +72,15 @@ public class UICooking : UIView
 
         for (int i = 0, count = _inventory[0].ItemsCount; i < count; i++)
         {
-            _uiCookingSlots[i].UpdateUI(_inventory[0].GetInventoryList()[i]);
+            _uiCookingSlots[i].UpdateUI(_inventory[0].Items[i]);
         }
+
+        _leftCookwareChangeButton.onClick.AddListener(() => ChangeCookware(-1));
+        _rightCookwareChangeButton.onClick.AddListener(() => ChangeCookware(1));
+        _leftCookwareChangeButton.gameObject.SetActive(false);
+        _rightCookwareChangeButton.gameObject.SetActive(true);
+
+        SetCookwareCount(0);
     }
 
     public void UpdateUI()
@@ -66,18 +92,95 @@ public class UICooking : UIView
 
         for (int i = 0, count = _inventory[0].ItemsCount; i < count; i++)
         {
-            _uiCookingSlots[i].UpdateUI(_inventory[0].GetInventoryList()[i]);
+            _uiCookingSlots[i].UpdateUI(_inventory[0].Items[i]);
         }
     }
 
-    public void StartCooking(RecipeData data)
-    {
-        _currentRecipeData = data;
 
-        gameObject.SetActive(false);
-        _uiCookingStart.gameObject.SetActive(true);
-        _uiCookingStart.StartCooking(_currentRecipeData);
+    private void StartCooking()
+    {
+        if(CookingSystem.IsEnabledCooking(_uiCookingCenterSlot[0].CurrentItem, _uiCookingCenterSlot[1].CurrentItem))
+        {
+            for(int i = 0, count = _uiCookingCenterSlot.Length; i < count; i++)
+            {
+                _uiCookingCenterSlot[i].ResetItem();
+            }
+            gameObject.SetActive(false);
+            _uiCookingStart.gameObject.SetActive(true);
+            _uiCookingStart.StartCooking(_currentRecipeData);
+        }
+
     }
+
+    private void ChangeCookware(int value)
+    {
+        int currentCookware = _cookingSystem.ChangeCookware(value);
+        _leftCookwareChangeButton.gameObject.SetActive(false);
+        _rightCookwareChangeButton.gameObject.SetActive(false);
+
+        _uiCookwares.ChangeImage(value, () =>
+        {
+            SetCookwareCount(currentCookware);
+            CheckCookEnabled();
+
+            if (currentCookware == 0)
+                _leftCookwareChangeButton.gameObject.SetActive(false);
+            else
+                _leftCookwareChangeButton.gameObject.SetActive(true);
+
+            if ((int)Cookware.Sizeof - 1 <= currentCookware)
+                _rightCookwareChangeButton.gameObject.SetActive(false);
+            else
+                _rightCookwareChangeButton.gameObject.SetActive(true);
+        });
+      
+    }
+
+
+    public void CheckCookEnabled()
+    {
+        if (CookingSystem.GetkRecipeByItems(_uiCookingCenterSlot[0].CurrentItem, _uiCookingCenterSlot[1].CurrentItem) != null)
+        {
+            _currentRecipeData = CookingSystem.GetkRecipeByItems(_uiCookingCenterSlot[0].CurrentItem, _uiCookingCenterSlot[1].CurrentItem);
+            HideButtonImage.SetActive(false);
+            return;
+        }
+        _hideButtonImage.SetActive(true);
+    }
+
+
+    public void ChoiceItem(InventoryItem item)
+    {
+        foreach (UICookingCenterSlot slot in _uiCookingCenterSlotList)
+        {
+            if (slot.CurrentItem != null)
+                continue;
+
+            slot.ChoiceItem(item);
+            return;
+        }
+
+        _uiCookingCenterSlotList.Last().ChoiceItem(item);
+        CheckCookEnabled();
+    }
+
+
+    private void SetCookwareCount(int value)
+    {
+        _uiCookingCenterSlotList.Clear();
+        for(int i = 0; i < _uiCookingCenterSlot.Length; i++)
+        {
+            _uiCookingCenterSlot[i].ResetItem();
+            _uiCookingCenterSlot[i].SetActiveSlot(false);
+        }
+
+        for(int i = 0; i < value + 1; i++)
+        {
+            _uiCookingCenterSlot[i].SetActiveSlot(true);
+            _uiCookingCenterSlotList.Add(_uiCookingCenterSlot[i]);
+        }
+    }
+
 
     public override void Show()
     {
---
+++ Assets/Scripts/Story/InteractionStoryEvent.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+[Tooltip("대화 사이에 이벤트를 실행하는 클래스")]
+
+public abstract class InteractionStoryEvent : StoryEvent
+{
+    [SerializeField] private FollowButton _followButtonPrefab;
+    private FollowButton _followButton;
+    public FollowButton FollowButton => _followButton;
+
+    [SerializeField] private Sprite _buttonImage;
+
+    [Tooltip("버튼이 현재 클래스 오브젝트에서 얼마만큼 떨어져있어야 하는지")]
+    [SerializeField] private Vector3 _buttonPos;
+
+    protected Action _nextActionHandler;
+
+    protected virtual void Start()
+    {
+        Vector3 targetPos = transform.position + _buttonPos;
+        Transform parent = GameObject.Find("Story Event Follow Button Parent").transform;
+        _followButton = Instantiate(_followButtonPrefab, transform.position + Vector3.up, Quaternion.identity, parent);
+        _followButton.Init(gameObject, targetPos, new Vector2(120, 120), _buttonImage, OnFollowButtonClicked);
+        HideFollowButton();
+    }
+
+
+    protected abstract void OnFollowButtonClicked();
+
+
+    protected void ShowFollowButton()
+    {
+        _followButton?.gameObject.SetActive(true);
+    }
+
+    protected void HideFollowButton()
+    {
+        _followButton?.gameObject.SetActive(false);
+    }
+
+}
