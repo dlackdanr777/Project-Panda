77ab995d12d1f67bddd716f63b0e08a4e71efb12
pushssun <alsgp25@naver.com>
2024-01-08 00:42:02 +0900

    [Refactor] 채집 도감
    채집 도감 리펙토링
    received 이미지 확인, 상세창 생성

--- Assets/Scripts/Book/UIBookList.cs
+++ Assets/Scripts/Book/UIBookList.cs
@@ -1,14 +1,134 @@
+using Muks.DataBind;
 using System;
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class UIBookList : MonoBehaviour
 {
     //토글이 선택되면 Book에서 해당 그림으로 변경
     //아이템 보여짐
     //상세창..
-    
+    [SerializeField] private InventoryItemField _itemField; //현재 어떤 필드의 아이템들인지
+    [SerializeField] private int _typeField; //해당 필드의 몇번째 아이템들인지
+    [SerializeField] private GameObject _detailView;
+    [SerializeField] private Button _closeButton;
+    [SerializeField] private Button _leftButton;
+    [SerializeField] private Button _rightButton;
 
+    private List<Item> _database;
+    private int _current; //현재 페이지 위치
+
+    private void OnDisable()
+    {
+        if (_detailView.activeSelf)
+        {
+            _detailView.SetActive(false);
+        }
+    }
+    private void Start()
+    {
+        //현재 어떤 필드의 몇 번째 아이템인지 데이터 연결
+        switch (_itemField)
+        {
+            case InventoryItemField.GatheringItem:
+                if(_typeField == 0)
+                {
+                    _database = DatabaseManager.Instance.GetBugItemList();
+                }
+                else if(_typeField == 1)
+                {
+                    _database = DatabaseManager.Instance.GetFishItemList();
+                }
+                else if(_typeField == 2)
+                {
+                    _database = DatabaseManager.Instance.GetFruitItemList();
+                }
+                else
+                {
+                    Debug.Log("해당 데이터가 없습니다.");
+                }
+            break;
+            case InventoryItemField.Cook:
+                break;
+            case InventoryItemField.Tool:
+                break;
+        }
+
+        _current = 0;
+        for(int i=0;i<transform.childCount;i++)
+        {
+            int index = i;
+            Button button = transform.GetChild(i).GetComponent<Button>();
+            button.onClick.AddListener(()=> OnClickDetailView(index));
+        }
+        _closeButton.onClick.AddListener(() => _detailView.SetActive(false));
+        _leftButton.onClick.AddListener(()=>OnClickPageButton(0));
+        _rightButton.onClick.AddListener(()=>OnClickPageButton(1));
+
+
+        UpdateContents();
+    }
+
+    private void UpdateContents()
+    {
+        for(int i = 0; i < transform.childCount; i++)
+        {
+            Transform child = transform.GetChild(i);
+            if ((_current*9) + i >= _database.Count)
+            {
+                child.gameObject.SetActive(false);
+            }
+            else
+            {
+                child.gameObject.SetActive(true);
+                if (_database[(_current * 9) + i].IsReceived)
+                {
+                    child.GetChild(0).GetComponent<Image>().sprite = _database[(_current*9) + i].Image;
+                    child.GetComponent<Button>().interactable = true;
+                }
+                else
+                {
+                    child.GetChild(0).GetComponent<Image>().sprite = null;
+                    child.GetComponent<Button>().interactable = false;
+                }
+
+            }
+        }
+    }
+
+    private void OnClickPageButton(int arrow) //arrow: 0 이면 왼쪽, 1이면 오른쪽
+    {
+        if(arrow == 0)
+        {
+            _current--;
+            if(_current < 0)
+            {
+                _current = 0;
+            }
+        }
+        else if(arrow == 1)
+        {
+            _current++;
+            if(_current > _database.Count / 9)
+            {
+                _current = _database.Count/9;
+            }
+        }
+        UpdateContents();
+    }
+
+    private void OnClickDetailView(int index)
+    {
+        _detailView.SetActive(true);
+        GetContent(index);
+    }
+
+    private void GetContent(int index)
+    {
+        DataBind.SetTextValue("BookItemDetailName", _database[(_current * 9) + index].Name);
+        DataBind.SetTextValue("BookItemDetailDescription", _database[(_current * 9) + index].Description);
+        DataBind.SetSpriteValue("BookItemDetailImage", _database[(_current * 9) + index].Image);
+    }
 
 }
