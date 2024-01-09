492a1845ef15379a58ea75ac1fae31eb72793888
pushssun <alsgp25@naver.com>
2024-01-09 16:17:50 +0900

    [Delete] Test code 삭제

--- Assets/Scripts/Inventory/UIInventoryContent.cs
+++ Assets/Scripts/Inventory/UIInventoryContent.cs
@@ -49,17 +49,6 @@ public class UIInventoryContent : UIList<InventoryItem, InventoryItemField>
 
     private void Awake()
     {
-        //Test
-        GameManager.Instance.Player.GatheringItemInventory[0].AddById(InventoryItemField.GatheringItem, (int)GatheringItemType.Bug, "IBG03");
-        GameManager.Instance.Player.GatheringItemInventory[0].AddById(InventoryItemField.GatheringItem, (int)GatheringItemType.Bug, "IBG03");
-        GameManager.Instance.Player.GatheringItemInventory[1].AddById(InventoryItemField.GatheringItem, (int)GatheringItemType.Fish, "IFI04");
-        GameManager.Instance.Player.GatheringItemInventory[2].AddById(InventoryItemField.GatheringItem, (int)GatheringItemType.Fruit, "IFR05");
-        GameManager.Instance.Player.GatheringItemInventory[1].AddById(InventoryItemField.GatheringItem, (int)GatheringItemType.Fish, "IFI04");
-        GameManager.Instance.Player.GatheringItemInventory[1].AddById(InventoryItemField.GatheringItem, (int)GatheringItemType.Fish, "IFI24");
-
-        GameManager.Instance.Player.ToolItemInventory[0].AddById(InventoryItemField.Tool, (int)ToolItemType.GatheringTool, "ITG03");
-        GameManager.Instance.Player.ToolItemInventory[0].AddById(InventoryItemField.Tool, (int)ToolItemType.GatheringTool, "ITG06");
-
         //미리 생성 => spawn 계속하면 안좋음
         for (int i = 0; i < System.Enum.GetValues(typeof(InventoryItemField)).Length - 1; i++)
         {
