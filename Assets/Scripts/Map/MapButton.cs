27b862d9409940b04aab8cf7abc73523ac285826
hye20 <minji11014@gmail.com>
2024-03-19 15:19:40 +0900

    [Update] Map

--- Assets/Scripts/Map/MapButton.cs
+++ Assets/Scripts/Map/MapButton.cs
@@ -2,7 +2,7 @@ using UnityEngine;
 using UnityEngine.UI;
 using Muks.Tween;
 using Muks.DataBind;
-
+using BT;
 
 public class MapButton : MonoBehaviour
 {
@@ -283,6 +283,9 @@ public class MapButton : MonoBehaviour
                 Camera.main.transform.position = targetPos + new Vector3(-lx, 0, 0);
             }
 
+            Vector3 pandaPosition = new Vector3(_targetTransform[CurrentMap].position.x + Random.Range(-_width + 1, _width - 1), _targetTransform[CurrentMap].position.y - 12, StarterPanda.Instance.gameObject.transform.position.z);
+            StarterPanda.Instance.gameObject.transform.position = pandaPosition;
+
             FadeInOutManager.Instance.FadeOut(_fadeTime);
         }));
     }
@@ -302,6 +305,9 @@ public class MapButton : MonoBehaviour
 
             Camera.main.transform.position = targetPos - new Vector3(lx, 0, 0);
 
+            Vector3 pandaPosition = new Vector3(_targetTransform[CurrentMap].position.x + Random.Range(-_width + 1, _width - 1), _targetTransform[CurrentMap].position.y - 12, StarterPanda.Instance.gameObject.transform.position.z);
+            StarterPanda.Instance.gameObject.transform.position = pandaPosition;
+
             FadeInOutManager.Instance.FadeOut(_fadeTime);
         }));
     }
@@ -317,6 +323,9 @@ public class MapButton : MonoBehaviour
             _cameraController.MapCenter = _targetTransform[(int)this.CurrentMap].position;
 
             Camera.main.transform.position = targetPos;
+
+            Vector3 pandaPosition = new Vector3(_targetTransform[CurrentMap].position.x + Random.Range(-_width + 1, _width - 1), _targetTransform[CurrentMap].position.y - 12, StarterPanda.Instance.gameObject.transform.position.z);
+            StarterPanda.Instance.gameObject.transform.position = pandaPosition;
 
             FadeInOutManager.Instance.FadeOut(_fadeTime);
         }));
